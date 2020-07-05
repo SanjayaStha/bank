310726cf86f6421f91d0c054e8912ac90244075a
sanjaya <ssanjayaiw@gmail.com>
2020-07-05 18:39:54 +0545

    [update] home page UI

---
+++ index.php
@@ -0,0 +1,327 @@
+
+<?php
+  include ('header.php');
+  include ('navbar.php');
+?>
+<SECTION>
+  <div class="container-fluid">
+    
+    <div class="row">
+      <div class="col-sm-8">
+      
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active slider">
+                <img src="images/laptop2.jpg" style="height:100%" class="img-thumbnail img-fluid" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="images/laptop1.jpg" style="height:100%" class="img-thumbnail img-fluid" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="images/note.jpg" style="height:100%" class="img-thumbnail img-fluid" alt="...">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div> 
+
+      </div>
+
+      <div class="col">
+      
+        <div class="row bg-dark slider-hover" style="height: 33.3333%">
+        
+          <div class="col">
+          <a href="">
+          <div class="centered">Our Banking Services...</div>
+          
+          </div>
+          </a>
+          <div class="col">
+            <img src="images/laptop2.jpg" style="width: 100%" alt="">
+          </div>
+          
+        </div>
+        
+        <div class="row bg-light slider-hover" style="height: 33.3333%">
+        <div class="col">
+        <div class="centered">Our Banking Services</div>
+          </div>
+          <div class="col">
+            <img src="images/laptop2.jpg" style="width: 100%" alt="">
+          </div>
+        </div>
+        <div class="row bg-dark slider-hover" style="height: 33.3333%">
+        <div class="col">
+        <div class="centered">Our Banking Services</div>
+          </div>
+          <div class="col">
+            <img src="images/laptop2.jpg" style="width: 100%" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+      <!-- <div class="col-lg-10 col-md-10 col-10 mx-auto my-5">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="images/laptop2.jpg" class="d-block w-100 img-thumbnail img-fluid" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="images/laptop1.jpg" class="d-block w-100 img-thumbnail img-fluid" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="images/note.jpg" class="d-block w-100 img-thumbnail img-fluid" alt="...">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div> 
+          </div>    -->
+      
+
+    </div>
+</SECTION>
+</header>
+
+
+<div class="breaker mt-3"></div>
+<section>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="section-title">Introduction</div>
+    </div>
+    <div class="row">
+    <div class="col">
+      <img src="images/laptop1.jpg" width="100%" alt="">
+    </div>
+    <div class="col ml-5">
+      <div class="section-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nulla pariatur provident neque odit ipsa ab quae animi praesentium tempora, reiciendis accusantium temporibus velit at atque officiis quidem voluptatibus asperiores eaque? Repudiandae officia sit beatae eaque molestiae esse porro et facilis eius, harum, ex, excepturi doloribus magni accusamus? Optio, fugit.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eos officiis nulla mollitia. Saepe sint architecto aspernatur quisquam sit, autem beatae fugit quibusdam hic debitis similique ratione aliquam inventore. Ea accusamus sint temporibus quos deleniti ipsa quis iure nisi, tempora quas omnis dolores provident consequuntur corrupti ad cupiditate suscipit nam?
+      <br>
+      <button class="btn btn-success">Learn More</button>
+      </div>
+
+    </div>
+      </div>
+      </div>
+</section>
+
+<div class="breaker mt-3"></div>
+<section>
+  <div class="container-fluid">
+  <div class="row">
+      <div class="section-title">Our Activities</div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+      <div class="row">
+        <div class="article-title"><a href="">Lorem ipsum dolor sit amet con</a></div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs"><img src="images/laptop1.jpg" style="width:250px" alt=""></div>
+        
+        <div class="col">  
+          <div class="section-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, veritatis voluptatibus asperiores exercitationem soluta repudiandae, vel laborum fugit animi, expedita repellat illo atque officia ab.
+            <br>
+            <button class="btn btn-success">Read More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+    <div class="col">
+      <div class="row"><div class="article-title"><a href="">Lorem ipsum dolor sit amet con</a></div></div>
+       
+      <div class="row">
+         <div class="col-xs"><img src="images/laptop1.jpg" style="width:250px" alt=""></div>
+        <div class="col">  
+          <div class="section-content"> facilis eius, harum, ex, excepturi doloribus magni accusamus? Optio, fugit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut magni nulla alias a reprehenderit animi officiis id quidem! Soluta, nam.
+          <br>
+          <button class="btn btn-success">Read More</button>
+        
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+  <div class="container"><button class="btn btn-outline-success float-right">See More</button></div>
+  </div>
+
+</section>
+
+<div class="breaker mt-3"></div>
+<section>
+  <div class="container-fluid">
+  <div class="row">
+      <div class="section-title">Our Remittances</div>
+  </div>
+  <div class="row">
+  <!-- <div class="section-content"> asperiores eaque? Repudiandae officiat.</div> -->
+
+  </div>
+  <div class="row">
+                    <div class="col credit-item">
+                        <a href="#"><img src="images/ime.png" class="img-responsive" alt=""></a>
+                    </div>
+                    <div class="col credit-item">
+                        <a href="#"><img src="images/prabhu.png" class="img-responsive" alt=""></a>
+                        </div>
+                    <div class="col credit-item">
+                        <a href="#"><img src="images/himal.jpg" width="207" height="208" class="img-responsive" alt=""></a>
+                    </div>
+                    <div class="col credit-item">
+                        <a href="#"><img src="images/muktinath.png" width="207" height="208" class="img-responsive" alt=""></a>
+                    </div>
+  </div>
+
+  <div class="row">
+    <div class="container"><button class="btn btn-outline-success float-right">See More</button></div>
+  </div>
+    </div>
+</section>
+
+
+<div class="breaker mt-3"></div>
+<section>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="section-title">Our Services</div>
+    </div>
+    <div class="row">
+    <!-- <div class="section-content">Lorem ipsum dolor sit amet.</div> -->
+    <div class="card ml-auto" style="width: 18rem;">
+    <img class="card-img-top" src="images/laptop1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title article-title" >Savings</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-outline-success">Go somewhere</a>
+    </div>
+  </div>
+
+  <div class="card ml-auto" style="width: 18rem;">
+    <img class="card-img-top" src="images/laptop1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title article-title">Loan</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-outline-success">Go somewhere</a>
+    </div>
+  </div>
+
+  <div class="card ml-auto" style="width: 18rem;">
+    <img class="card-img-top" src="images/laptop1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title article-title">Remittance</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-outline-success">Go somewhere</a>
+    </div>
+  </div>
+
+  <div class="card ml-auto" style="width: 18rem;">
+    <img class="card-img-top" src="images/laptop1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title article-title">Micro Finance</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-outline-success">Go somewhere</a>
+    </div>
+  </div>
+
+    </div>
+    <div class="row">
+    <div class="container"><button class="btn btn-outline-success float-right">See More</button></div>
+  </div>
+  </div>
+</section>
+
+
+<div class="breaker mt-3"></div>
+
+<section>
+  <div class="credit-section" id="services">
+              <div class="container-fluid">
+                  <div class="row">
+                      <div class="col">
+                          <h2 class="section-title"><span class="eng">Credit Types</span><span class="nep"> ऋण प्रकार</h2></span>
+                      </div>
+                  </div>
+                  <div class="row credit-row">
+                      <div class="col credit-item" id="credit1">
+                        <div class="credit-item credit-item1">
+                          <h2><span class="eng">Overdraft</span><span class="nep"></h2></span>
+                          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis iure, tempore, sunt cupiditate, numquam optio voluptatem ut quae vitae recusandae aperiam cum excepturi repellat eveniet? Adipisci ducimus saepe ratione iure sint placeat architecto at illum quas nesciunt? Veniam, voluptas nobis.</p>
+                        
+                        </div>
+                      
+                      </div>
+                      <!-- ---------- -->
+                      <div class="col credit-item" id="credit1">
+                        <div class="credit-item credit-item1">
+                          <h2><span class="eng">Agriculture Loan</span><span class="nep"></h2></span>
+                          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis iure, tempore, sunt cupiditate, numquam optio voluptatem ut quae vitae recusandae aperiam cum excepturi repellat eveniet? Adipisci ducimus saepe ratione iure sint placeat architecto at illum quas nesciunt? Veniam, voluptas nobis.</p>
+                        
+                        </div>
+                      
+                      </div>
+
+                      <!-- ---------0 -->
+
+                      <div class="col credit-item" id="credit1">
+                        <div class="credit-item credit-item1">
+                          <h2><span class="eng">Consumer Loan</span><span class="nep"></h2></span>
+                          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis iure, tempore, sunt cupiditate, numquam optio voluptatem ut quae vitae recusandae aperiam cum excepturi repellat eveniet? Adipisci ducimus saepe ratione iure sint placeat architecto at illum quas nesciunt? Veniam, voluptas nobis.</p>
+                        
+                        </div>
+                      
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!-- <div class="message-section" id="about">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-md-8 col-md-offset-2 wow fadeInDown animated">
+                          <h2 class="section-title">News and Event</h2>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      </div>
+                  </div>
+              </div>
+          </div> -->
+</section>
+
+<?php
+  include ('footer.php');
+?>
+
+
+ 
